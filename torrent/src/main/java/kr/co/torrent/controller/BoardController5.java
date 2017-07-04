04149fed6f5d0c4de69402cadf22d1a908bdd3df
Bit503-20-PC\Bit503-20 <ksb304@naver.com>
2017-07-04 17:47:08 +0900

    awdasd

---
+++ torrent/src/main/java/kr/co/torrent/controller/BoardController5.java
@@ -0,0 +1,13 @@
+package kr.co.torrent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BoardController5 {
+
+	@RequestMapping("home")
+	public void home(){
+		
+	}
+}
