fcb2b75f7ea302bbf84b75c77522a5f1cb4fee04
Bit503-20-PC\Bit503-20 <ksb304@naver.com>
2017-07-04 17:35:34 +0900

    awd

---
+++ torrent/src/main/java/kr/co/torrent/interceptor/AuthInterceptor.java
@@ -0,0 +1,25 @@
+package kr.co.torrent.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import kr.co.torrent.vo.UserVO;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession();
+		UserVO member = (UserVO) session.getAttribute("user");
+		if (member != null) {
+			return true;
+		}
+		response.sendRedirect("loginForm.do");
+		return false;
+	}
+
+}
--- torrent/src/main/java/kr/co/torrent/vo/BoardVO.java
+++ torrent/src/main/java/kr/co/torrent/vo/BoardVO.java
@@ -1,5 +1,52 @@
 package kr.co.torrent.vo;
 
+import java.util.Date;
+
 public class BoardVO {
 
+	private int bno;
+	private String title;
+	private String content;
+	private String id;
+	private Date regDate;
+	private int genre;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public Date getRegDate() {
+		return regDate;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public int getGenre() {
+		return genre;
+	}
+	public void setGenre(int genre) {
+		this.genre = genre;
+	}
+	
+	
 }
